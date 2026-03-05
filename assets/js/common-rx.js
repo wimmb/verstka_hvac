f1db3565044fe095afbf29d22433c2e80c6c2d20
snaker <veremeenko.ig@gmail.com>
2026-03-05 15:30:28 +0200

    cont-3

--- assets/js/common-rx.js
+++ assets/js/common-rx.js
@@ -5,12 +5,12 @@ $(document).ready(function () {
 	}*/
 
 	// Fancybox init
-	/*if (document.querySelector('[data-fancybox]')) {
+	if (document.querySelector('[data-fancybox]')) {
 		Fancybox.bind('[data-fancybox]', {
 			dragToClose: false,
 			closeButton: false,
 		});
-	}*/
+	}
 
     // Hero carousel swiper
     const hero__carousel = document.querySelector('.hero__carousel');
@@ -35,8 +35,6 @@ $(document).ready(function () {
 		});
 	}
 
-	
-
 	// Logos carousel swiper
 	const tracks = document.querySelectorAll('.hlogos__track');
 	if (tracks.length) {
@@ -47,27 +45,6 @@ $(document).ready(function () {
 			}
 		});
 	}
-
-	// Reviews carousel swiper
-	/*const reviews__carousel = document.querySelector('.reviews__carousel');
-	if (reviews__carousel) {
-		const swiper = new Swiper(reviews__carousel, {
-			slidesPerView: 'auto',
-			allowTouchMove: false,
-			loop: true,
-			speed: 1000,
-			autoplay: true,
-      		pagination: {
-      			el: '.reviews__pagination',
-      			clickable: true,
-    		},
-			navigation: {
-      			nextEl: '.reviews__btn-next',
-      			prevEl: '.reviews__btn-prev',
-    		},
-		});
-	}*/
-
 });
 
 // TwentyTwenty init
@@ -111,5 +88,40 @@ document.addEventListener('DOMContentLoaded', function () {
 	const links = document.querySelectorAll('a[href^="#"]:not([href="#"]), .scroll-btn');
 	links.forEach(link => {
 		link.addEventListener('click', handleLinkClick);
+	});
+});
+
+// Toggles
+document.addEventListener('DOMContentLoaded', function () {
+	const toggleBlocks = document.querySelectorAll('.block__toggle');
+
+	if (toggleBlocks.length === 0) {
+		return;
+	}
+
+	toggleBlocks.forEach(block => {
+		const header = block.querySelector('.toggle__header');
+		const button = block.querySelector('.toggle__header-btn');
+		const content = block.querySelector('.toggle__content');
+
+		if (!header || !button || !content) {
+			return;
+		}
+
+		header.addEventListener('click', function () {
+			if (content.style.maxHeight) {
+				content.style.maxHeight = null;
+				content.style.paddingBottom = null;
+				button.classList.remove('v_active');
+				content.classList.remove('c_active');
+				block.classList.remove('t_active');
+			} else {
+				content.style.maxHeight = content.scrollHeight + 20 + 'px';
+				content.style.paddingBottom = '20px';
+				button.classList.add('v_active');
+				content.classList.add('c_active');
+				block.classList.add('t_active');
+			}
+		});
 	});
 });
